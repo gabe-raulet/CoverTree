281b930c2d082f6c908d36b6ce695ab5c6edf289
Gabriel Raulet <gabe.h.raulet@gmail.com>
2025-06-08 14:35:22 -0700

    queries look good so far

--- brute_force.py
+++ brute_force.py
@@ -1,21 +1,25 @@
 import time
 import numpy as np
 import sklearn
+from sklearn.neighbors import KDTree, BallTree
 from sklearn.metrics.pairwise import euclidean_distances
 
 import cppimport
 import cppimport.import_hook
 from extensions.brute_force import distance_matrix
 
-n = 20000
-d = 32
+class BruteForce(object):
 
-np.random.seed(134)
+    def __init__(self, points):
+        self.points = points
+        self.dists = distance_matrix(points, num_threads=8)
+
+n = 20000
+d = 12
+
 points = np.random.uniform(0,1,size=(n,d))
 
-num_threads=12
+brute_force = BruteForce(points)
+tree = KDTree(points)
 
-t = -time.perf_counter()
-D = distance_matrix(points, num_threads=num_threads)
-t += time.perf_counter()
 
--- extensions/brute_force.cpp
+++ extensions/brute_force.cpp
@@ -2,6 +2,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <pybind11/stl.h>
+#include <stdexcept>
 #include <stdio.h>
 #include <array>
 #include <tuple>
@@ -50,10 +51,69 @@ py::array_t<double> distance_matrix(py::array_t<double> P_py, int num_threads = 
     return D_py;
 }
 
+void brute_force_knn_query(double *P, double *q, double *dists, int64_t *inds, int64_t n, int64_t d, int64_t k)
+{
+    static EuclideanDistance distance;
+
+    std::vector<std::tuple<double, int64_t>> results(n);
+
+    for (int64_t i = 0; i < n; ++i)
+    {
+        std::get<0>(results[i]) = distance(&P[i*d], q, d);
+        std::get<1>(results[i]) = i;
+    }
+
+    std::sort(results.begin(), results.end());
+
+    for (int64_t i = 0; i < k; ++i)
+    {
+        dists[i] = std::get<0>(results[i]);
+        inds[i] = std::get<1>(results[i]);
+    }
+}
+
+void brute_force_query_(double *P, double *X, double *dists, int64_t *inds, int64_t n, int64_t d, int64_t m, int64_t k)
+{
+    for (uint64_t i = 0; i < m; ++i)
+    {
+        brute_force_knn_query(P, &X[i*d], &dists[i*k], &inds[i*k], n, d, k);
+    }
+}
+
+void brute_force_query(py::array_t<double> P_py, py::array_t<double> X_py, py::array_t<double> dists_py, py::array_t<int64_t> inds_py, int num_threads)
+{
+    py::buffer_info Pinfo = P_py.request();
+    py::buffer_info Xinfo = X_py.request();
+    py::buffer_info distsinfo = dists_py.request();
+    py::buffer_info indsinfo = inds_py.request();
+
+    int64_t n = Pinfo.shape[0];
+    int64_t d = Pinfo.shape[1];
+
+    int64_t m = Xinfo.shape[0];
+    int64_t k = distsinfo.shape[1];
+
+    if (d != Xinfo.shape[1]) throw std::runtime_error("error: P.shape[1] != X.shape[1]");
+    else if (m != distsinfo.shape[0]) throw std::runtime_error("error: X.shape[0] != dists.shape[0] (m)");
+    else if (m != indsinfo.shape[0]) throw std::runtime_error("error: X.shape[0] != inds.shape[0] (m)");
+    else if (k != indsinfo.shape[1]) throw std::runtime_error("error: dists.shape[1] != inds.shape[1] (k)");
+    else if (k > n) throw std::runtime_error("error: k > P.shape[0]");
+
+    double *P = static_cast<double*>(Pinfo.ptr);
+    double *X = static_cast<double*>(Xinfo.ptr);
+    double *dists = static_cast<double*>(distsinfo.ptr);
+    int64_t *inds = static_cast<int64_t*>(indsinfo.ptr);
+
+    omp_set_num_threads(1);
+
+    brute_force_query_(P, X, dists, inds, n, d, m, k);
+}
+
 
 PYBIND11_MODULE(brute_force, m)
 {
     m.def("distance_matrix", &distance_matrix, py::arg("P_py"), py::arg("num_threads") = 4);
+    m.def("brute_force_query", &brute_force_query);
 }
 
 /*
