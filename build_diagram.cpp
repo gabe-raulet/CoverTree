1641365262a4915252c2d76e986ad4d30195187a
Gabriel Raulet <gabe.h.raulet@gmail.com>
2025-07-26 18:25:23 -0700

    looks good

---
+++ build_diagram.cpp
@@ -0,0 +1,43 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <iostream>
+
+#include "utils.h"
+#include "point_vector.h"
+#include "dist_voronoi.h"
+
+int main(int argc, char *argv[])
+{
+    int myrank, nprocs;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    if (argc != 3)
+    {
+        if (!myrank) fprintf(stderr, "Usage: %s <points> <centers>\n", argv[0]);
+        MPI_Finalize();
+        return 0;
+    }
+
+    const char *fname = argv[1];
+    Index centers = atoi(argv[2]);
+
+    GlobalPointVector points; points.read_fvecs(fname, MPI_COMM_WORLD);
+
+    {
+        double t, maxtime;
+
+        t = -MPI_Wtime();
+        DistVoronoi diagram(points, 0, MPI_COMM_WORLD);
+        diagram.add_next_centers(centers);
+        t += MPI_Wtime();
+
+        MPI_Reduce(&t, &maxtime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+        if (!myrank) printf("[time=%.3f] %s\n", maxtime, diagram.repr().c_str());
+    }
+
+    MPI_Finalize();
+    return 0;
+}
