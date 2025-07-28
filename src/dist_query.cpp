fca60998bc1fdd06cdaca63c6d24bd70e51631a9
Gabriel Raulet <gabe.h.raulet@gmail.com>
2025-07-28 12:38:48 -0700

    makes comparisons easier

--- src/dist_query.cpp
+++ src/dist_query.cpp
@@ -81,7 +81,8 @@ void DistQuery::write_to_file(const char *fname) const
 
     for (Index i = 0; i < num_local_queries_made; ++i)
         for (Index p = myptrs[i]; p < myptrs[i+1]; ++p)
-            ss << (myqueries[i]+1) << " " << (myneighs[p]+1) << "\n";
+            if (myqueries[i] >= myneighs[p])
+                ss << (myqueries[i]+1) << " " << (myneighs[p]+1) << "\n";
 
     auto sbuf = ss.str();
     std::vector<char> buf(sbuf.begin(), sbuf.end());
