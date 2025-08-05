ab737f119c49731512c15755cb5f28a8108c6aa2
Gabriel Raulet <gabe.h.raulet@gmail.com>
2025-08-05 13:50:41 -0700

    excellent

--- ptgen.py
+++ ptgen.py
@@ -3,14 +3,8 @@ import sys
 import numpy as np
 import math
 import time
-
-def write_file(points, fname):
-    assert points.dtype == np.float32
-    n, d = points.shape
-    with open(fname, "wb") as f:
-        for i in range(n):
-            f.write(d.to_bytes(4, byteorder="little"))
-            f.write(points[i].tobytes())
+from dataset_io import *
+from sklearn.datasets import make_blobs
 
 def random_rotation_matrix(d):
     A = np.random.normal(0, 1, (d, d))
@@ -21,18 +15,18 @@ def random_rotation_matrix(d):
 
     return Q
 
-def main(n, dim, ambient, fname):
-    p1 = np.random.normal(0, 1, (n, dim))
+def main(n, dim, ambient, centers, fname):
+    p1 = make_blobs(n_samples=n, n_features=dim, centers=centers)[0]
     p2 = np.zeros((n, ambient-dim))
     A = np.hstack([p1,p2])
     R = random_rotation_matrix(ambient)
     points = (A@R).astype(np.float32)
-    write_file(points, fname)
+    write_file(fname, points)
     return 0
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print(f"Usage: {sys.argv[0]} <n> <dim> <ambient> <fname>")
+    if len(sys.argv) != 6:
+        print(f"Usage: {sys.argv[0]} <n> <dim> <ambient> <centers> <fname>")
         sys.exit(1)
     else:
-        sys.exit(main(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), sys.argv[4]))
+        sys.exit(main(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), sys.argv[5]))
